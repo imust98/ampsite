7f3fb09ea871b85ebac2d4b6f391b601ad33c085
lihaoliang <hzlihaoliang@corp.netease.com>
2017-09-04 10:01:09 +0800

    优化动画效果

--- src/components/Process.vue
+++ src/components/Process.vue
@@ -1,54 +1,96 @@
 <template>
-  <div class="m-process">
-    <div class="app">
-      <img src="../assets/website_animation_app.png" alt="">
-      <p>APP</p>
-    </div>
-    <div class="dns">
-      <div class="an_0"></div>
-      <div class="an_1"></div>
-      <div class="an_2"></div>
-      <div class="an_3"></div>
-      <div class="an_4"></div>
-      <img src="../assets/dns/website_animation_DNS.png" alt="">
-      <p>DNS监测</p>
-    </div>
-    <div class="isp">
-      <div class="an_1"></div>
-      <div class="an_2">
-        <div class="an_2_1"></div>
+  <div class="m-slider">
+    <div class="slider">
+      <div class="slider_image slider_dns" v-bind:class="{active:index===0}">
+        <img src="../assets/dns/website_DNS_tips.png" alt="">
+        <div class="description">
+          <h4>DNS劫持监控</h4>
+          <p>全面监控DNS和内容劫持分析，帮助您及时发现劫持最严重的URL，并定位到劫持主机，让您的app远离劫持风险 </p>
+        </div>
       </div>
-      <div class="an_3"></div>
-      <div class="an_4"></div>
-      <img src="../assets/isp/website_animation_ISP.png" alt="">
-      <p>ISP监测</p>
+      <div class="slider_image slider_isp" v-bind:class="{active:index===1}">
+        <img src="../assets/isp/website_ISP_tips.png" alt="">
+        <div class="description">
+          <h4>ISP网络质量问题分析 </h4>
+          <p>精确分析网络出错和慢响应请求，并可通过具体的诊断信息，判断出现问题时的网络状态，方便问题归因。</p>
+        </div>
+      </div>
+      <div class="slider_image slider_cdn" v-bind:class="{active:index===2}">
+        <img src="../assets/cdn/website_CDN_tips.png" alt="">
+        <div class="description">
+          <h4>CDN性能分析</h4>
+          <p>深入了解CDN在每个地域和运营商下的表现、每个CDN节点的访问质量和调度情况，快速定位CDN常见问题，获得最优的加速效果</p>
+        </div>
+      </div>
     </div>
-    <div class="cdn">
-      <div class="an_0"></div>
-      <div class="an_1"></div>
-      <div class="an_2"></div>
-      <div class="an_3"></div>
-      <img src="../assets/cdn/website_animation_CDN.png" alt="">
-      <p>CDN监测</p>
-    </div>
-    <div class="server active">
-      <div class="an_0"></div>
-      <img src="../assets/website_animation_server.png" alt="">
-      <p>Server</p>
+    <div class="accordion">
+      <div class="app">
+        <img src="../assets/website_animation_app.png" alt="">
+        <p>APP</p>
+      </div>
+      <div class="dns" v-on:mouseenter="go(0)" v-bind:class="{active:index===0}">
+        <div class="an_0"></div>
+        <div class="an_1"></div>
+        <div class="an_2"></div>
+        <div class="an_3"></div>
+        <div class="an_4"></div>
+        <img src="../assets/dns/website_animation_DNS.png" alt="">
+        <p>DNS监测</p>
+      </div>
+      <div class="isp" v-on:mouseenter="go(1)" v-bind:class="{active:index===1}">
+        <div class="an_1"></div>
+        <div class="an_2">
+          <div class="an_2_1"></div>
+        </div>
+        <div class="an_3"></div>
+        <div class="an_4"></div>
+        <img src="../assets/isp/website_animation_ISP.png" alt="">
+        <p>ISP监测</p>
+      </div>
+      <div class="cdn" v-on:mouseenter="go(2)" v-bind:class="{active:index===2}">
+        <div class="an_0"></div>
+        <div class="an_1"></div>
+        <div class="an_2"></div>
+        <div class="an_3"></div>
+        <div class="an_4"></div>
+        <img src="../assets/cdn/website_animation_CDN.png" alt="">
+        <p>CDN监测</p>
+      </div>
+      <div class="server" v-bind:class="{active:index===3}">
+        <img src="../assets/website_animation_server.png" alt="">
+        <p>Server</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  //  require('../styles/keyframes.mcss');
   export default {
     name: 'process',
     data () {
       return {
-        msg: ''
+        index: 0,
+        time: 5000
       }
     },
-    methods: {}
+    methods: {
+      play: function () {
+        this.interval = setInterval(function () {
+          this.index++
+          if (this.index === 3) {
+            this.index = 0
+          }
+        }.bind(this), this.time)
+      },
+      stop: function () {
+        clearInterval(this.interval)
+      },
+      go: function (index) {
+        this.stop()
+        this.index = index
+        this.play()
+      }
+    }
   }
 </script>
 
@@ -56,10 +98,81 @@
 <style lang="scss" rel="stylesheet/scss">
   @import '../style/keyframes.scss';
 
-  .m-process {
+  .m-slider {
+    width: 1200px;
+  }
+
+  .slider {
+    position: relative;
+    width: 100%;
+    height: 255px;
+    margin: 0px auto;
+    .description {
+      position: absolute;
+      width: 250px;
+      top: 0px;
+      h4 {
+        padding-top: 20px;
+        font-size: 18px;
+        color: #333;
+        font-weight: normal;
+      }
+      p {
+        font-size: 12px;
+        line-height: 24px;
+        color: #838383;
+      }
+    }
+    .slider_image {
+      position: absolute;
+      img, .description {
+        display: none;
+      }
+      &.active {
+        z-index: 2;
+        img, .description {
+          display: block;
+        }
+        img {
+          animation: imageFadeIn 1s 0.5s both, moveUp 0.5s 0.5s ease-out both;
+        }
+        .description {
+          animation: imageFadeIn 1s 0.7s both, moveUp 0.5s 0.7s ease-out both;
+        }
+      }
+    }
+    .slider_dns {
+      left: 225px;
+      top: 0px;
+      .description {
+        left: 515px;
+      }
+    }
+    .slider_isp {
+      left: 260px;
+      top: 0px;
+      .description {
+        left: 480px;
+      }
+    }
+    .slider_cdn {
+      left: 455px;
+      top: 0px;
+      .description {
+        left: -255px;
+        h4 {
+          text-align: right;
+        }
+      }
+    }
+  }
+
+  .accordion {
     display: flex;
+    justify-content: center;
     .app, .dns, .isp, .cdn, .server {
       position: relative;
+      cursor: pointer;
     }
     @mixin ems {
       position: absolute;
@@ -71,17 +184,17 @@
 
     .dns {
       .an_0 {
+        @include ems;
+        top: 140px;
+        left: -60px;
+        background: url(../assets/website_animation_file01.png);
+      }
+      .an_1 {
         position: absolute;
         width: 30px;
         height: 16px;
         left: 215px;
         top: 100px;
-      }
-      .an_1 {
-        @include ems;
-        top: 140px;
-        left: -60px;
-        background: url(../assets/website_animation_file01.png);
       }
       .an_2 {
         position: absolute;
@@ -100,7 +213,6 @@
         left: 101px;
         background: url(../assets/dns/animation_DNS_symbol01.png);
       }
-
       .an_4 {
         position: absolute;
         width: 29px;
@@ -112,19 +224,19 @@
       }
       &.active {
         .an_0 {
-          animation: light1 0.8s linear 5;
+          animation: trans60 1s ease-out, fadeInOut 1s ease-out forwards;
         }
         .an_1 {
-          animation: trans60 1s, fadeIn 1s ease-in forwards;
+          animation: light1 0.8s 1s linear 5;
         }
         .an_2 {
-          animation: trans60 1s 1s, fadeIn 1s 1s ease-in forwards;
+          animation: trans60 1s 1s, fadeInOut 1s 1s ease-in forwards;
         }
         .an_3 {
-          animation: shake3 0.2s 2s linear 5;
+          animation: shake 0.2s 2s linear 10;
         }
         .an_4 {
-          animation: trans30 1s 3s, fadeIn 1s 3s ease-in forwards;
+          animation: trans30 1s 4s, fadeInOut 1s 4s ease-in forwards;
         }
       }
     }
@@ -168,7 +280,7 @@
       }
       &.active {
         .an_1 {
-          animation: trans60 1s, fadeIn 1s linear forwards;
+          animation: trans60 1s, fadeInOut 1s linear forwards;
         }
         .an_2 {
           animation: retate20 2s 1s linear alternate 2;
@@ -177,7 +289,7 @@
           animation: light2 0.8s 1s linear 5;
         }
         .an_3 {
-          animation: shake1 0.2s 1s linear 20;
+          animation: shake 0.2s 1s linear 20;
         }
         .an_4 {
           animation: light3 0.8s 1s linear 5;
@@ -217,31 +329,27 @@
         top: 50px;
         background: url(../assets/cdn/animation_CDN_symbol03.png);
       }
-      &.active {
-        .an_0 {
-          animation: trans60 1s, fadeIn 1s linear forwards;
-        }
-        .an_1 {
-          animation: retate30 2s 1s linear forwards 2;
-        }
-        .an_2 {
-          animation: retate30 2s 1.5s linear forwards 2;
-        }
-        .an_3 {
-          animation: moveDown 2s 1s linear forwards 2;
-        }
-      }
-    }
-    .server {
-      .an_0 {
+      .an_4 {
         @include ems;
-        left: -80px;
+        left: 230px;
         top: 150px;
         background: url(../assets/website_animation_file04.png);
       }
       &.active {
         .an_0 {
-          animation: trans60 1s, fadeIn 1s linear forwards;
+          animation: trans60 1s, fadeInOut 1s linear forwards;
+        }
+        .an_1 {
+          animation: retate30 2s 1s linear forwards 2;
+        }
+        .an_2 {
+          animation: retate30 1.5s 2s linear forwards 2;
+        }
+        .an_3 {
+          animation: moveDown 2s 1s linear forwards 2;
+        }
+        .an_4 {
+          animation: trans60 1s 4s, fadeInOut 1s 4s linear forwards;
         }
       }
     }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Process from './components/Process'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: {App},
-  created: function () {
-    console.log(22)
+  el: '#PRO',
+  components: {Process},
+  mounted: function () {
+    this.$refs.pro.play()
   }
 })
